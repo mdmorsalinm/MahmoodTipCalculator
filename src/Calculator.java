6511abe5e6fea1d9f01397894709899eda4f541a
Morsalin <morsalinmahmood@gmail.com>
2023-09-23 21:18:32 -0400

    freestyle done, added cash and change

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,9 +11,13 @@ public class Calculator {
         System.out.print("What's the tip percentage? (0-100): ");
         int tip = scan.nextInt();
         scan.nextLine();
+        System.out.print("How much money do you have?: "); // Freestyle
+        double cash = scan.nextDouble();
+        scan.nextLine();
+
         double cost = 0.0;
         double totalCost = 0.0;
-        ArrayList<String> items = new ArrayList<String>();
+        ArrayList<String> items = new ArrayList<>();
         while (cost != -1) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             cost = scan.nextDouble();
@@ -28,10 +32,12 @@ public class Calculator {
         }
         System.out.println("-------------------------------------------");
         double totalTip = totalCost * (tip / 100.0);
+        System.out.println("Balance: $" + cash);
         System.out.println("Total bill before tip: $" + totalCost);
         System.out.println("Total percentage: " + tip + "%");
         System.out.println("Total tip: $" + (Math.round(totalTip * 100.0) / 100.0));
         System.out.println("Total bill with tip: $" + (Math.round((totalTip + totalCost) * 100.0) / 100.0));
+        System.out.println("Your change: $" + (Math.round((cash - (totalTip + totalCost)) * 100.0) / 100.0)); // Freestyle
         System.out.println("Per person cost before tip: $" + (Math.round((totalCost / group) * 100.0) / 100.0));
         System.out.println("Tip per person: $" + (Math.round((totalTip / group) * 100.0) / 100.0));
         System.out.println("Total cost per person: $" + (Math.round(((totalCost + totalTip) / group) * 100.0) / 100.0));
@@ -41,5 +47,8 @@ public class Calculator {
             System.out.println(oneItem);
         }
         scan.close();
+
+        // I learned how to use list from https://stackoverflow.com/questions/2843366/how-to-add-new-elements-to-an-array
+        // I learned how to round from https://stackoverflow.com/questions/8825209/rounding-decimal-points
     }
 }
