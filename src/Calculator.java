749bb48da13082ab8940d3f8f749f6cc32477ce5
Morsalin <morsalinmahmood@gmail.com>
2023-09-30 22:09:13 -0400

    final edits

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.ArrayList; // lists
 
 public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to the Tip Calculator!");
+        // greeting
+        System.out.println("Welcome to Morsalin's Tip Calculator!");
         System.out.print("How many people are in your group: ");
         int group = scan.nextInt();
-        scan.nextLine();
+        scan.nextLine(); // extra line so code runs as intended
+        // asks for percentage of tip from total cost
         System.out.print("What's the tip percentage? (0-100): ");
         int tip = scan.nextInt();
-        scan.nextLine();
+        scan.nextLine(); // extra line so code runs as intended
         System.out.print("How much money do you have?: "); // Freestyle
         double cash = scan.nextDouble();
-        scan.nextLine();
+        scan.nextLine(); // extra line so code runs as intended
 
+        // these variables will be changed in the while loop
         double cost = 0.0;
         double totalCost = 0.0;
-        ArrayList<String> items = new ArrayList<>();
+        ArrayList<String> items = new ArrayList<>(); // list of items ordered
+        // this loop asks the user for items they order and their price
+        // runs till user inputs -1
         while (cost != -1) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             cost = scan.nextDouble();
@@ -25,11 +30,13 @@ public class Calculator {
 
             if (cost != -1) {
                 totalCost += cost;
+                // extra credit
                 System.out.print("Enter the item: ");
                 String item = scan.nextLine();
                 items.add(item);
             }
         }
+        // Calculations
         System.out.println("-------------------------------------------");
         double totalTip = totalCost * (tip / 100.0);
         System.out.println("Balance: $" + cash);
@@ -42,6 +49,7 @@ public class Calculator {
         System.out.println("Tip per person: $" + (Math.round((totalTip / group) * 100.0) / 100.0));
         System.out.println("Total cost per person: $" + (Math.round(((totalCost + totalTip) / group) * 100.0) / 100.0));
         System.out.println("-------------------------------------------");
+        // extra credit
         System.out.println("Items ordered:");
         for (String oneItem : items) {
             System.out.println(oneItem);
